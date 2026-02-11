2333861e7e6f2cad446f80f70b7c0f4837425bce
Alexander Dokuchaev <doker.al@yandex.com>
2026-02-11 19:40:52 +0300

    version 0.13.0

--- core/commonTest/ValidatorExample.kt
+++ core/commonTest/ValidatorExample.kt
@@ -4,17 +4,9 @@ object Person {
     val age = DynoKey<Int>("age").validate {
         require(it < 0) { "'age' must be positive" }
     }
-    val age2 by dynoKey<Int>().validate {
-        require(it < 0) { "'age' must be positive" }
-    }
-    val age3 by dynoKey<Int?>().validate {
-        require(it < 0) { "'age' must be positive" }
-    }
     val name by dynoKey<String?>().notBlank()
-    val name2 by dynoKey<String>().notBlank()
 }
 
 fun <R: DynoKeySpec<String>> R.notBlank() = validate {
     require(it.isNotBlank()) { "property '$name' must not be empty" }
 }
-
